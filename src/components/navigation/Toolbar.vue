5a9898f5e8b9d64a467c64832373625c8b5bdce5
Jacob Adams <Artchecks@gmail.com>
2019-03-15 10:09:18 -0700

    project init

---
+++ src/components/navigation/Toolbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-toolbar
+    app
+    dark
+    clipped-left
+    color="success"
+  >
+   <!-- if design wants a nav drawer use below code.
+    <v-toolbar-side-icon @click="toggleNavigationDisplay"></v-toolbar-side-icon> -->
+    <v-toolbar-title>{{ path }}</v-toolbar-title>
+    <v-spacer />
+    <v-toolbar-title></v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+  methods: {
+    // if team wants nav drawer
+    // toggleNavigationDisplay () {
+    //   this.$store.commit('toggleNavigationDisplay')
+    // }
+  },
+  computed: {
+    getPath () {
+      return this.$store.getters.getPath
+    },
+    path () {
+      if(this.getPath === 'Home') {
+        return 'Git: A guide to Version Control Software'
+      } else {
+        return 'Git: ' + this.getPath
+      }
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify, {
+  iconfont: 'md'
+})
 
 new Vue({
   store,
