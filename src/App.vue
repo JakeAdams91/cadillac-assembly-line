dd4ac108e066294a233eea54729ae823c2bb291d
Jacob Adams <Artchecks@gmail.com>
2019-03-15 10:24:09 -0700

    foundation begun of site

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <v-app>
     <toolbar />
+    <!-- <nav-drawer /> -->
     <HomePage/>
   </v-app>
 </template>
 
 <script>
 import Toolbar from './components/navigation/Toolbar'
+// import NavDrawer from './components/navigation/NavDrawer'    -- if used
 import HomePage from './components/HomePage.vue'
 export default {
   name: 'app',
   components: {
     HomePage,
-    Toolbar
+    Toolbar,
+    // NavDrawer   -- if used
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -1,14 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    // --- for navigation drawer if used ---
+    // NavigationDisplay: false,
+    path: 'Home'
+  },
+  getters: {
+    // ---- for navigation drawer if used ----
+    // getNavigationDisplay (state) {
+    //   return state.NavigationDisplay
+    // },
+    getPath (state) {
+      return state.path
+    }
   },
   mutations: {
-
+    // ---- for navigation drawer if used ----
+    // toggleNavigationDisplay (state) {
+    //   state.NavigationDisplay = !state.NavigationDisplay
+    // },
+    setPath (state, newPath) {
+      state.path = newPath
+    }
   },
   actions: {
 
