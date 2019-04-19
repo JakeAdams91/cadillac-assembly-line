9208c53b32531da4c3ead5a774246b99432b4b07
Jacob Adams <Artchecks@gmail.com>
2019-04-18 20:54:31 -0700

    improved calendar functionality0

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -8,7 +8,51 @@
           </v-card-title>
           <v-flex offset-xs6 xs6>
             <v-card>
-              <v-calendar></v-calendar>
+            <v-calendar>
+              <template v-slot:day="{ date }">
+                <template v-for="event in eventsMap[date]">
+                  <v-menu
+                    :key="event.venue"
+                    v-model="event.open"
+                    full-width
+                    offset-x
+                  >
+                    <template v-slot:activator="{ on }">
+                      <div
+                        v-if="!event.time"
+                        v-ripple
+                        class="my-event"
+                        v-on="on"
+                        v-html="event.venue"
+                      ></div>
+                    </template>
+                    <v-card
+                      color="grey lighten-4"
+                      min-width="350px"
+                      flat
+                    >
+                      <v-toolbar
+                        color="#4372AA"
+                        dark
+                      >
+                        <v-toolbar-title v-html="event.venue + ' ' + dateFormat(event.date)"> </v-toolbar-title>
+                      </v-toolbar>
+                      <v-card-title primary-title>
+                        <span v-html="event.details"></span>
+                      </v-card-title>
+                      <v-card-actions>
+                        <v-btn
+                          flat
+                          color="#687C97"
+                        >
+                          Close
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-menu>
+                </template>
+              </template>
+              </v-calendar>
             </v-card>
           </v-flex>
         </v-card>
@@ -19,10 +63,58 @@
 
 <script>
 export default {
-  name: 'Calendar'
+  name: 'Calendar',
+  data () {
+    return {
+      months: ['error: ', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    }
+  },
+  computed: {
+    getDates () {
+      return this.$store.getters.getDates
+    },
+    upcomingShows () {
+      return this.$store.getters.getUpcomingShows
+    },
+    // sort events by date and create new objects of each upcoming event.
+    eventsMap () {
+      const map = {}
+      this.upcomingShows.forEach(e => (map[e.date] = map[e.date] || []).push(e))
+      return map
+    }
+  },
+  methods: {
+    // receives the event's date, 
+    // reformats it to 3 letter Month and day int.
+    // i.e. 2019-04-20 becomes  Apr 20
+    dateFormat (month) {
+      let split = month.split('-')
+      let day = split[2]
+      let monthNum = split[1]
+      monthNum = monthNum.substring(1)
+      let monthy = this.months[monthNum]
+      let newFormat = '- ' + monthy + ' ' + day
+      // console.log(monthy)
+      return newFormat
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.my-event {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    border-radius: 2px;
+    background-color: #4372AA;
+    color: #ffffff;
+    border: 1px solid #1867c0;
+    width: 100%;
+    font-size: 12px;
+    padding: 3px;
+    cursor: pointer;
+    margin-bottom: 1px;
+  }
 </style>
--- src/store.js
+++ src/store.js
@@ -6,15 +6,27 @@ export default new Vuex.Store({
   state: {
     // --- for navigation drawer if used ---
     // NavigationDisplay: false,
+    // default page
     path: 'Home',
-    paths: ['Home', 'About Us', 'Upcoming Shows', 'Contact Us'],
+    // list of pages
+    paths: ['Home', 'About Us', 'Upcoming Shows'],
+    // each band member objectified
     band: [
       { name: 'Jim', img: require('./assets/images/Jim-6.jpg'), bio: 'Jim has a masters degree in Music, ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores' },
       { name: 'Paul', img: require('./assets/images/paul.jpg'), bio: 'ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores' },
       { name: 'Sab', img: require('./assets/images/sab.jpg'), bio: 'ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores' },
       { name: 'Doug', img: require('./assets/images/doug.jpg'), bio: 'ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores' },
       { name: 'Vince', img: require('./assets/images/vince.jpg'), bio: 'ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores' }  
-    ]
+    ],
+    // venues played // upcoming.
+    upcomingShows: [
+      { date: '2019-04-20', venue: 'The Blues Foundation', img: require('./assets/bluesfoundation.png'), details: 'ut perspiciatis unde omnis iste natus error sit voluptatem ' },
+      { date: '2019-04-28', venue: 'Moutain View Pub', img: require('./assets/mountainviewpub.png'), details: 'ut perspiciatis unde omnis iste natus error sit voluptatem ' },
+      { date: '2019-04-30', venue: 'Phoenix blues Society', img: require('./assets/phoenixbluessociety.png'), details: 'ut perspiciatis unde omnis iste natus error sit voluptatem ' },
+      { date: '2019-04-23', venue: 'The Rhythm Room', img: require('./assets/rhythmroom.png'), details: 'ut perspiciatis unde omnis iste natus error sit voluptatem ' },
+      { date: '2019-05-01', venue: 'West Alley BBQ', img: require('./assets/westalleybbq.png'), details: 'ut perspiciatis unde omnis iste natus error sit voluptatem ' }
+    ],
+    dates: [ '2019-04-20', '2019-04-28', '2019-04-30', '2019-04-23' ]
   },
   getters: {
     // ---- for navigation drawer if used ----
@@ -29,6 +41,12 @@ export default new Vuex.Store({
     },
     getBand (state) {
       return state.band
+    },
+    getUpcomingShows (state) {
+      return state.upcomingShows
+    },
+    getDates (state) {
+      return state.dates
     }
   },
   mutations: {
