5c7687d009f2cd41d1878ec8026ea4c23604f927
Jacob Adams <Artchecks@gmail.com>
2019-04-20 14:25:21 -0700

    removed deprecated dependancies

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import Vuetify from 'vuetify'
-import VeeValidate from 'vee-validate'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vuetify/dist/vuetify.min.css'
 
@@ -10,7 +9,7 @@ Vue.config.productionTip = false
 Vue.use(Vuetify, {
   iconfont: 'md'
 })
-Vue.use(VeeValidate)
+
 new Vue({
   store,
   render: h => h(App)
