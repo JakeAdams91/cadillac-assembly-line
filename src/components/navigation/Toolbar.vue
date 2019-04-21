5c4dccbfbec3b120236830819172a3c7d7c1c1bf
Jacob Adams <Artchecks@gmail.com>
2019-04-21 07:54:39 -0700

    implemented updated Logo

--- src/components/navigation/Toolbar.vue
+++ src/components/navigation/Toolbar.vue
@@ -1,19 +1,19 @@
 <template>
-  <!-- give toolbar absolute to have toolbar dissapear @ scroll. -->
-  <!-- absolute -->
+  <!-- header/toolbar -->
   <v-toolbar
     app
     dark
-    
     scroll-off-screen
     :scroll-threshold="10"
     color="#B0C4DE"
   >
+  <!-- create logo -->
     <v-flex xs6 >
-      <v-img class="mt-5" :src="require('../../assets/logos/logo3.png')" width="350" height="128"></v-img>
+      <v-img class="mt-5" :src="require('../../assets/logos/logo3-1.png')" width="350" height="128"></v-img>
     </v-flex>
     <template v-slot:extension>
       <v-spacer/>
+      <!-- buttons to navigate the site -->
       <v-toolbar-items>
         <v-btn flat @click="setPath('Home')">Home</v-btn>
         <v-btn flat @click="setPath('About Us')">About Us</v-btn>
